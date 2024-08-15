4f9d802a9b7a2bb39e777b4a1c162f22a4a11dbd
abhaybhadouriya <sbhadouriya39@gmail.com>
2024-08-16 00:41:51 +0530

    15-Aug

---
+++ Question6.c
@@ -0,0 +1,45 @@
+#include <unistd.h>
+
+int main() {
+    char maxBufferSpace[500];
+    // Diff between stdin vs STDIN_FILENO
+    // 1. stdin - is just a file pointer to ' FIle * ' defined in <stdio.h>
+    //    it uses higher level I/O function like fscanf and fgets and fread
+    // 2. STDIN_FILENO is an integer constant representing the FD for standard input in <unistd.h>
+    //    typically has the value `0`
+    //    it uses low level system calls like read and write
+    //    STDIN_FILE - interact with OS directly bypassing standard library buffering
+    //    so we have direct control over I/O
+
+    // abstraction 
+    // std -> highlevel abstraction offers buffered I/O - slow
+    // stdin_fileno -> unbuffered I/O , direct access to system I/O,LL abstraction - faster
+    // Buffered I/O -> instead of transferring data immediately to or from file or device
+    // data is first stored in a buffer, once it is full then data is transferred all at once
+    // by doing this system calls are redunce but performance of program is decreased
+    // and for reading 'fgets' and 'fread' instead of making system call to fetach data immediatley
+    // it reads the block of data and requested data return to the program and process so on
+
+
+    // there are 3 FD 
+    //     1. STDIN_FILENO (0){stdin} - take input from keyboard or pipe etc
+    //     2. STDOUT_FILENO (1){stdout} -  write o/p
+    //     3. STDERR_FILENO (2){stderr} - for error messages writing 
+
+    // read is LL function used read from FD such as file,pipe. it is unbuffered
+    // on success return the no of bytes it reads. 
+    printf("Write Something \n");
+    ssize_t bytesReadSize = read(STDIN_FILENO, maxBufferSpace, maxBufferSpace);
+
+    // Writing with 
+    // write(fd,buff[],size);
+    // this method is used to write in a file using FD 
+    // 3rd arg is provided to specify the length of data we are going to write
+    // on succress return the no of bytes it actually returned
+    // it is block I/O - it block the program untill it the data is written to the file or device
+    // handle partial writes  - if it block by system then check if no bytes  are fewer then requested then just retry
+    write(STDOUT_FILENO, maxBufferSpace, bytesReadSize);
+    
+
+    return 0;
+}
---
+++ Question8.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#define MAX_BUFFER_SIZE 10240
+
+int main() {
+    // give a name for a file to point
+    // and open it with O_RDONLY access
+    // ######################
+    // a note for TAs please change the file name in your pc if your are checking
+    int fd = open("testfile_Q7.txt", O_RDONLY); 
+    // if fd is less than 0 that means there is an error
+    if (fd < 0) {
+        perror("There is an Error  while opening  the file");
+        return 1;
+    }
+    // declaring the array of char to store tthe char byte by byte
+    char buffer[MAX_BUFFER_SIZE];
+    // declaring ssize_t varibale to track record the no of with to read or readed from file 
+    ssize_t bytesRead, i = 0;
+    // declaring the line valriable and initilizing to 1
+    int line =1;
+    // now reading the while using read fucntion 
+    // here we  passed the FD and buffer and buffer size
+    bytesRead = read(fd, buffer, MAX_BUFFER_SIZE);
+    // itareting through byte by byte or char by char to check a new line char "\n"
+        for (ssize_t j = 0; j < bytesRead; ++j) {
+            // if we encounter the new line char just enter in it
+            if (buffer[j]== '\n') {
+                // calculate the lenght from which index to which we have to read
+                int len = j-i+1;
+                // now print the line with line no 
+                // and print the string using char[] by defining the line size
+                // for 1st it will run from 0 to the first new line character' \n'
+                // then it increament the i index from 0 to j+1 for next line and so on
+                printf("%d : %.*s\n",line++,len,buffer+i);
+                // incrementing the i index to start of new linel
+                i = j+1;
+            }
+        }
+    // of this varible is less then it means there is an error while reading
+        if (bytesRead < 0) {
+        perror("Please check again there is an Error while reading this file");
+        // closing the FD and exiting with code 0
+        close(fd);
+        // return error code
+        return 1;
+    }
+    // close fd
+    close(fd);
+    // return success code 
+    return 0;
+}

9cf7f6344554dcf23abcde84e5d5ea46e627919d
abhaybhadouriya <sbhadouriya39@gmail.com>
2024-08-20 20:24:07 +0530

    Q4

--- Question23.c
+++ Question23.c
@@ -1,3 +1,6 @@
+// Roll no :- MT2024003
+// Name :- Abhay Bhadouriya
+// 23. Write a program to create a Zombie state of the running program.
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,25 +11,29 @@ int main() {
 
     if (pid < 0) {
         // Fork failed
-        perror("Fork failed");
-        exit(EXIT_FAILURE);
+        perror("There is an error in fork - process is not created");
+        return 1;
     } else if (pid == 0) {
         // Child process
-        printf("Child process (PID: %d) is exiting...\n", getpid());
-        exit(EXIT_SUCCESS);  // Exit immediately, becoming a zombie
+        printf("Child process  with process ID: %d is completed now exiting.\n", getpid());
+        exit(0);  // Exit immediately, becoming a zombie
     } else {
         // Parent process
-        printf("Parent process (PID: %d) will now sleep...\n", getpid());
-        printf("Child process (PID: %d) will become a zombie...\n", pid);
-
-        // Sleep for 30 seconds to allow observation of the zombie state
+        printf("Parent process with Process ID: %d is running will now sleep.\n", getpid());
+        // Sleep for 30 seconds to check the zombie state in terminal 
+        // run this process FG or BG use below command to check zombie process
+        // $ps aux | grep Z
+        // here parent is sleeping for 30 sec without calling wait()
+        // so child become zombie because it is exited but still not yet acknowledged by parent
         sleep(30);
 
-        // Optionally, the parent process can clean up by calling wait()
-        // wait(NULL);
-
-        printf("Parent process (PID: %d) is exiting...\n", getpid());
+       
+        printf("Parent process with process ID: %d is exiting.\n", getpid());
     }
 
     return 0;
 }
+
+// zombie process is a process that is completed but still has an entry in process table.
+// this happen due to parent process did not use 'wait()' or 'waitpid()' function to check exit status of child
+// 

03acd1913358edcec680db8dbe9b901b9deba89c
abhaybhadouriya <sbhadouriya39@gmail.com>
2024-08-21 01:45:47 +0530

    Q29

--- Question28.c
+++ Question28.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <sched.h>
 #include <unistd.h>
+void printPri(char *string,int SCHED_POLICY){
+printf("Max realtime priority of %s Scheduler is : %d\n",string, sched_get_priority_max(SCHED_POLICY));
+printf("Min realtime priority of %s Scheduler is : %d\n",string, sched_get_priority_min(SCHED_POLICY));
+}
+
 int main() {
     int pid;
     int policy;
@@ -12,11 +17,12 @@ int main() {
     // SCHED_FIFO
     // SCHED_RR
     // SCHED_OTHER
-    // SCHED_BATCH
-    // SCHED_IDLE
     // so to get correct priory we need current scheduling algo
-    printf("Max realtime priority of program is : %d\n", sched_get_priority_max(SCHED_FIFO));
-    printf("Min realtime priority of program is : %d\n", sched_get_priority_min(SCHED_FIFO));
+    printPri("SCHED_FIFO",SCHED_FIFO);
+    printPri("SCHED_RR",SCHED_RR);
+    printPri("SCHED_OTHER",SCHED_OTHER);
    
     return 0;
 }
+
+
---
+++ Question29.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sched.h>
+#include <errno.h>
+
+void printCurrentPolicy(int policy) {
+    switch (policy) {
+        case SCHED_OTHER:
+            printf("Current : SCHED_OTHER\n");
+            break;
+        case SCHED_FIFO:
+            printf("Current : SCHED_FIFO \n");
+            break;
+        case SCHED_RR:
+            printf("Current : SCHED_RR\n");
+            break;
+        default:
+            printf("Unknown scheduling policy\n");
+            break;
+    }
+}
+
+int main() {
+    int pid = getpid();  
+    struct sched_param param;
+    printCurrentPolicy(sched_getscheduler(pid));
+    param.sched_priority = 10;
+    sched_setscheduler(pid, SCHED_FIFO, &param);
+    printf("Scheduling policy changed to SCHED_FIFO with priority %d\n", param.sched_priority);
+    printCurrentPolicy(sched_getscheduler(pid));
+    param.sched_priority = 15; 
+    sched_setscheduler(pid, SCHED_RR, &param);
+    printf("Scheduling policy changed to SCHED_RR with priority %d\n", param.sched_priority);
+    printCurrentPolicy(sched_getscheduler(pid));
+
+    return 0;
+}
